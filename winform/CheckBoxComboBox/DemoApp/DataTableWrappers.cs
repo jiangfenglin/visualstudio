fb32ae984ae476b91ad27a3302f613dc3b126097
jiangfenglin <jiangfenglin@live.com>
2017-10-07 20:45:37 +0800

    CheckBoxComboBox

---
+++ winform/CheckBoxComboBox/DemoApp/DataTableWrappers.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+using PresentationControls;
+
+namespace DemoApp
+{
+    public class DataTableWrapper : ListSelectionWrapper<DataRow>
+    {
+        public DataTableWrapper(DataTable dataTable, string usePropertyAsDisplayName) : base(dataTable.Rows, false, usePropertyAsDisplayName) { }
+    }
+}
